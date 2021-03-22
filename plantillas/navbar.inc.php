95e15f280f36972450097256b2326433b25008e5
luchiano9 <67933124+luchiano9@users.noreply.github.com>
2021-03-21 22:42:19 -0300

    Add files via upload
    Plantillas para declaración y cierre de las páginas y la barra de navegación primera versión.
    Index actualizado.

---
+++ plantillas/navbar.inc.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-expand-lg navbar-static-top navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Entornos</a>
+      <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-expanded="false" aria-controls="navbarTogglerDemo02" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="navbar-collapse collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Alumnos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Docentes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Consultas</a>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Foro</a>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Contacto</a>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Ayuda</a>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Buscar</button>
+        </form>
+      </div>
+    </div>
+  </nav>
